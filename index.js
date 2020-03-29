3af8704b2561fd1e4f0565714386062e5c0cc381
ThePersigg <49908303+perssBest@users.noreply.github.com>
2020-03-29 19:53:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require("http")
+const link = require('./endpoints.json').supportServer
 
 module.exports = async(content) => {
     return new Promise((resolve, reject) => {
-        if(!content) return resolve(require('./endpoints.json'))
+        if(!content) return resolve('If you have any errors, please contact the project administration ' + link)
         const url = `http://miss.glitch.me/api/${content}`
         http.get(url, function(res){
             let body = " ";
@@ -13,3 +14,7 @@ module.exports = async(content) => {
         })
     })
 }
+
+module.exports.version = require('./package.json').version
+
+/* Developer with ❤ by !Няфа💔 | Няя#1510 */
